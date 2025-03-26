b9278fa2bf1daf26da2a859daf77cf4c0a5d78c4
vanessaputtri <cinemas.vanessa.9c.28@gmail.com>
2025-03-26 09:04:26 +0800

    mengupload tugas minggu lalu, ga ngerti git dibantu teman

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.test.ui.theme.TestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstamceState)
+        setContentView(R.layout.activity_main)
+    }
+}
